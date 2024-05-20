549f430653fbfcd259b8dadc2eb797c9b69a0e17
pizamooo <dobliws@gmail.com>
2024-05-20 22:12:20 +0300

    Creation Python and PyQt Designer files.

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
-print("hello")
+import sqlite3
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+
+db_name = sqlite3.connect('databases/mydb.db')
+cursor = db_name.cursor()
+def on_click():
+    print("hello")
+
+app = QApplication([])
+Form, Window = uic.loadUiType("MainForm.ui")
+window = Window()
+form = Form()
+form.setupUi(window)
+form.log_in.clicked.connect(on_click)
+window.show()
+db_name.close()
+app.exec()
