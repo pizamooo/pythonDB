f4635f4bbe478c11b7c4d399c06b09fc72d60b01
pizamooo <dobliws@gmail.com>
2024-05-21 10:29:25 +0300

    Edit Python and Ui file.

--- database.py
+++ database.py
@@ -4,15 +4,14 @@ from PyQt6.QtWidgets import QApplication
 
 db_name = sqlite3.connect('databases/mydb.db')
 cursor = db_name.cursor()
-def on_click():
-    print("hello")
 
 app = QApplication([])
 Form, Window = uic.loadUiType("MainForm.ui")
 window = Window()
 form = Form()
 form.setupUi(window)
-form.log_in.clicked.connect(on_click)
 window.show()
+
+db_name.commit()
 db_name.close()
 app.exec()
